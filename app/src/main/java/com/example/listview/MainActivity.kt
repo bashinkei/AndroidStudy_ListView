6cb88752a674fa9a291de052af66c6314e9d5873
bashinkei <bashinkei@gmail.com>
2019-02-15 00:07:32 +0900

    finish!

--- app/src/main/java/com/example/listview/MainActivity.kt
+++ app/src/main/java/com/example/listview/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.example.listview
 
+import android.icu.util.TimeZone
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //画面のレイアウトを固定する
         setContentView(R.layout.activity_main)
+
+        //リスtpで表示するタイムゾーン一覧
+        val timeZones = TimeZone.getAvailableIDs()
+
+        //リストをレイアウトから探す
+        val listView = findViewById<ListView>(R.id.timeZoneList)
+
+        //アダプターを作成
+        val adapter = ArrayAdapter<String>(
+            this,
+            R.layout.list_time_zone_row,
+            R.id.timezone,
+            timeZones
+        )
+
+        //リストにアダプターをセットする
+        listView.adapter = adapter
+
+        //リストのアイテムタップ時の動作
+        listView.setOnItemClickListener { parent, view, position, id ->
+            //  アダプターから押された位置のタイムゾーンを得る
+            val timeZone = adapter.getItem(position)
+
+            //Toastで表示
+            Toast.makeText(this, timeZone, Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
